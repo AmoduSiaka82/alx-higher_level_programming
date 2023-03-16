1ec27acc4c347c4353848a0cc0e310fafb731d40
AmoduSiaka82 <amodusiakanet@gmail.com>
2023-03-15 19:27:01 -0700

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
-        if k == key:
-            d = {k: value}
-            a_dictionary.update(d)
-        else:
-            d = {key: value}
-            a_dictionary.update(d)
+    d = {key: value}
+    a_dictionary.update(d)
     return (a_dictionary)
